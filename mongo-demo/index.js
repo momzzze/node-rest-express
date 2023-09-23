becfb4d73bcbf6ae311ee43f33b8484cafcb0b93
nikola.ninov <momzzze@abv.bg>
2023-09-23 20:37:25 +0300

    Update index.js

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
 
 mongoose.connect('mongodb://localhost/playground')
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch(err => console.error("Could not connect to MongoDB...", err));
+    .then(() => console.log("Connected to MongoDB..."))
+    .catch(err => console.error("Could not connect to MongoDB...", err));
 
-    const courseSchema = new mongoose.Schema({
-        name: String,
-        author: String,
-        tags: [String],
-        date: { type: Date, default: Date.now },
-        isPublished: Boolean,
-    });
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean,
+});
 
 const Course = mongoose.model("Course", courseSchema);
 
@@ -27,4 +27,14 @@ async function createCourse() {
     console.log(result);
 }
 
-createCourse();
+const getCourses = async () => {
+    const courses = await Course
+        .find({ author: "Nikola", isPublished: true })
+        .limit(10)
+        .sort({ name: 1 })
+        .select({ name: 1, tags: 1 });
+    console.log(courses);
+}
+
+getCourses();
+// createCourse();
