1878baaf512cb156e0d3b594b47c1b4646731927
nikola.ninov <momzzze@abv.bg>
2023-12-10 21:06:03 +0200

    add JWT  access token

--- api-exercise/express-js-auth-apis/Routes/Auth.route.js
+++ api-exercise/express-js-auth-apis/Routes/Auth.route.js
@@ -4,22 +4,22 @@ const router = express.Router();
 const createError = require('http-errors');
 const User = require('../Models/User.model');
 const { authSchema } = require('../helpers/validation_schema');
-
+const { signAccessToken } = require('../helpers/jwt_helper');
 
 router.post('/register', async (req, res, next) => {
     try {
         // const { email, password } = req.body;
         // if (!email || !password) throw createError.BadRequest();
         const result = await authSchema.validateAsync(req.body);
-        
+
         const doesExist = await User.findOne({ email: result.email })
 
         if (doesExist) throw createError.Conflict(`${result.email} is already been registered`);
 
         const user = new User(result);
         const savedUser=await user.save();
-
-        res.send(savedUser);
+        const accessToken=await signAccessToken(savedUser.id);
+        res.send({accessToken});
 
 
     } catch (error) {
