de3797dc2003d0a228ed9422f1e5bf3b281bc5e7
nikola.ninov <momzzze@abv.bg>
2023-12-24 14:21:57 +0200

    book - create/index/new , index front page view books

--- rest-api/server.js
+++ rest-api/server.js
@@ -6,9 +6,10 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
-const bodyParser=require('body-parser');
+const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index');
-const authorRouter=require('./routes/author');
+const authorRouter = require('./routes/author');
+const bookRouter = require('./routes/books');
 
 // middlewares
 app.set('view engine', 'ejs');
@@ -17,7 +18,7 @@ app.set('layout', 'layouts/layout');
 
 app.use(expressLayouts);
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({limit:'10mb',extended:false}));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 // db connection
 const mongoose = require('mongoose');
@@ -36,8 +37,8 @@ connectDb();
 
 // routes
 app.use('/', indexRouter);
-app.use('/authors',authorRouter);
-
+app.use('/authors', authorRouter);
+app.use('/books', bookRouter);
 
 
 app.listen(process.env.PORT || 3000, () => {
