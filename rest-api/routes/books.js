45aaf73a76216c1a4dcb803522b7f7edac7864f5
nikola.ninov <momzzze@abv.bg>
2023-12-26 10:42:49 +0200

    add FilePond
    we remove multer since we use encoded string instead of file

--- rest-api/routes/books.js
+++ rest-api/routes/books.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
+// const multer = require('multer');
 const fs = require('fs')
 const Book = require('../models/book');
 const Author = require('../models/author');
 const path = require('path');
 const uploadPath = path.join('public', Book.coverImageBasePath);
 const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
-const upload = multer({
-    dest: uploadPath,
-    fileFilter: (req, file, callback) => {
-        callback(null, imageMimeTypes.includes(file.mimetype));
-    }
-})
+// const upload = multer({
+//     dest: uploadPath,
+//     fileFilter: (req, file, callback) => {
+//         callback(null, imageMimeTypes.includes(file.mimetype));
+//     }
+// })
 
 // All books route
 router.get('/', async (req, res) => {
@@ -47,7 +47,7 @@ router.get('/new', async (req, res) => {
 
 
 // Create Book Route
-router.post('/', upload.single('cover'), async (req, res) => {
+router.post('/',  async (req, res) => {
     const fileName = req.file != null ? req.file.filename : null
     const book = new Book({
         title: req.body.title,
