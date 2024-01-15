0a1d1d16db61ebd8055a32a8df9ca95da922ebb9
nikola.ninov <momzzze@abv.bg>
2024-01-15 13:36:19 +0200

    change server to render one

--- chat-room/server/public/app.js
+++ chat-room/server/public/app.js
@@ -1,4 +1,4 @@
-const socket = io(`ws://localhost:3500`);
+const socket = io(`https://chat-room-service-fdi9.onrender.com`);
 
 const msgInput = document.querySelector('#message');
 const nameInput = document.querySelector('#name');
