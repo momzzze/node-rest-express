c5fe279d833b699240c5c1945ca99a202b31d04c
nikola.ninov <momzzze@abv.bg>
2024-01-16 19:02:18 +0200

    create user done with validation and password hashing
    postman tests are done

--- chat-app/server/Controllers/userController.js
+++ chat-app/server/Controllers/userController.js
@@ -1,7 +1,53 @@
 const userModel = require('../Models/userModel');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const validator = require('validator');
 
-const registerUser = (req, res) => {
-    res.send("Register");
+const createToken = (_id) => {
+    const jwtKey = process.env.SECRET_KEY;
+    return jwt.sign({ _id }, jwtKey, {
+        expiresIn: 3 * 24 * 60 * 60
+    })
+}
+
+const registerUser = async (req, res) => {
+    // we will geting name email and the password from the user
+    const { name, email, password } = req.body;
+
+    let user = await userModel.findOne({ email });
+
+    if (user) {
+        return res.status(400).json({ error: "User already exists" });
+    }
+    if (!name || !email || !password) {
+        return res.status(400).json({ error: "Please fill all the fields" });
+    }
+    if (!validator.isEmail(email)) {
+        return res.status(400).json({ error: "Please enter a valid email" });
+    }
+    if (!validator.isStrongPassword(password)) {
+        return res.status(400).json({ error: "Password must be a strong password..." });
+    }
+
+    // if the user does not exist then we will create a new user
+    try {
+        user = new userModel({
+            name,
+            email,
+            password
+        });
+        // password hashing
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt)
+
+        await user.save();
+        const token = createToken(user._id);
+
+        res.status(200).json({ _id: user._id, name, email, token });
+
+    } catch (error) {
+        return res.status(400).json({ error: "Something went wrong" });
+    }
 }
 
 module.exports = {
