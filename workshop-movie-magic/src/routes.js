a6618c663d1c89eb35f0140ff9b96ab6016d102b
nikola.ninov <momzzze@abv.bg>
2024-01-17 19:44:57 +0200

    add 404 page

--- workshop-movie-magic/src/routes.js
+++ workshop-movie-magic/src/routes.js
@@ -2,8 +2,14 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
 const movieController = require('./controllers/movieController');
+
 router.use(homeController);
 router.use(movieController);
+
+router.get('*', (req, res) => {
+    res.render('404');
+});
+
 
 
 module.exports = router;
