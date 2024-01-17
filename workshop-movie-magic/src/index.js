1fef88fb6757338ed0b71e5dbe7180183d91053e
nikola.ninov <momzzze@abv.bg>
2024-01-17 18:39:51 +0200

    add and configure express-handlebars

--- workshop-movie-magic/src/index.js
+++ workshop-movie-magic/src/index.js
@@ -1,8 +1,15 @@
 const express=require('express');
-
+const handlebars=require('express-handlebars');
+const path=require('path');
 const app=express();
 
-const port=5000;
+app.engine('hbs',handlebars.engine({
+    extname:'hbs',
+}));
+app.set('view engine','hbs');
+app.set('views',path.join(__dirname,'views'));
+
+const port=5001;
 app.get('/',(req,res)=>{
     res.send('Hello world');
 });
