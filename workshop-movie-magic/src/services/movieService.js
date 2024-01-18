76b233e90a533c0a20ad83e8537857656bb786c9
nikola.ninov <momzzze@abv.bg>
2024-01-18 10:52:01 +0200

    persist created movie in database.json

--- workshop-movie-magic/src/services/movieService.js
+++ workshop-movie-magic/src/services/movieService.js
@@ -1,5 +1,43 @@
-const movies = [];
+// const movies = [
+//     {
+//         title: 'Jungle Cuise',
+//         genre: 'Adventure',
+//         director: 'John Doe',
+//         date: '2019',
+//         image: '/img/jungle-cruise.jpeg',
+//         rating: '5',
+//         description: 'Description: Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.'
+//     }
+// ];
+
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+const moviesFilePath = path.join(__dirname, '../config/database.json');
+
+const readFromDb = () => {
+    try {
+        const data = fs.readFileSync(moviesFilePath, 'utf-8');
+        return JSON.parse(data);
+    } catch (error) {
+        return []
+    }
+}
+const writeToDb = (data) => {
+    fs.writeFileSync(moviesFilePath, JSON.stringify(data, null, 2), 'utf8');
+}
 
 exports.create = (movieData) => {
-    movies.push(movieData);
+    const movie = {
+        id: uuidv4(),
+        ...movieData
+    }
+    const existingMovies = readFromDb();
+    
+    if (existingMovies.some(movie => movie.title === movieData.title)) {
+        return;
+    }
+    existingMovies.push(movie);
+    writeToDb(existingMovies);
 }
