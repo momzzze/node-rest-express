20e580a07c5823e0a9981aaa48e9c2c944cdab72
nikola.ninov <momzzze@abv.bg>
2024-01-18 09:45:39 +0200

    split config options for express and handlebars

---
+++ workshop-movie-magic/src/config/configExpress.js
@@ -0,0 +1,9 @@
+const express=require('express');
+const path=require('path');
+
+function configExpress(app){
+    app.use(express.static(path.resolve('src/public')));
+    return app;
+}
+
+module.exports = configExpress;
--- workshop-movie-magic/src/index.js
+++ workshop-movie-magic/src/index.js
@@ -1,17 +1,15 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
-const path = require('path');
+
+const routers= require('./routes');
+const configHandlebars = require('./config/configHandlebars');
+const configExpress = require('./config/configExpress');
+
 const app = express();
 const port = 5001;
-const routers= require('./routes');
 
+configHandlebars(app);
+configExpress(app);
 
-app.engine('hbs', handlebars.engine({
-    extname: 'hbs',
-}));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(routers);
 
 
