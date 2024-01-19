1e53bf7c78c17b49cfd2df0d017dc147fbf5a55c
nikola.ninov <momzzze@abv.bg>
2024-01-19 12:14:25 +0200

    add list chats
    build hook to get recipient

--- chat-app/public/src/pages/Chat.jsx
+++ chat-app/public/src/pages/Chat.jsx
@@ -1,15 +1,31 @@
 import { useContext } from "react"
 import { ChatContext } from "../context/ChatContext"
+import { Container, Stack } from "react-bootstrap"
+import UserChat from "../components/chat/UserChat"
+import { AuthContext } from "../context/AuthContext"
 
 const Chat = () => {
+    const { user } = useContext(AuthContext);
     const { userChats, isUserChatsLoading, userChatsErro } = useContext(ChatContext)
 
-    console.log("userChats", userChats);
-    console.log("isUserChatsLoading", isUserChatsLoading);
-    console.log("userChatsErro", userChatsErro);
-    return (
-        <div>Chat</div>
-    )
+
+    return <Container>
+        {userChats?.length < 1 ? null : (
+            <Stack direction="horizontal" gap={4} className="align-items-start">
+                <Stack className="messages-box flex-grow-0 pe-3" gap={3}>
+                    {isUserChatsLoading && <p>Loading...</p>}
+                    {userChats?.map((chat, index) => {
+                        return (
+                            <div key={index}>
+                                <UserChat chat={chat} user={user} />
+                            </div>
+                        )
+                    })}
+                </Stack>
+                <p>ChatBox</p>
+            </Stack>
+        )}
+    </Container>
 }
 
 export default Chat
