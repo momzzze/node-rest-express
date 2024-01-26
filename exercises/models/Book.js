dc0742768cd490f02befbbece007dd5815f39416
nikola.ninov <momzzze@abv.bg>
2024-01-26 15:09:36 +0200

    create book model

---
+++ exercises/models/Book.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        min: 3,
+    },
+    description: {
+        type: String,
+        required: true,
+        min: 10,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        validate: /^https?/,
+    },
+    publishedDate: {
+        type: Date,
+        required: true,
+    },
+    genre: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Genre',
+    },    
+    author: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Author',
+    },
+    comments: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'Comment',
+    }],
+   
+},
+{
+    timestamps: true,
+})
+
+const Book = mongoose.model('Book', bookSchema);
+module.exports = Book;
