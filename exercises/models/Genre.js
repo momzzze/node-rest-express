f4648406ebdcd2185651709387a7baed5e49d7f3
nikola.ninov <momzzze@abv.bg>
2024-01-26 15:14:00 +0200

    create Genre schema

---
+++ exercises/models/Genre.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const genreSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        min: 3,
+    },
+});
+
+const Genre = mongoose.model('Genre', genreSchema);
+module.exports = Genre;
