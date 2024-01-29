f1363e751ff636053a1a5b130257d790592c6162
nikola.ninov <momzzze@abv.bg>
2024-01-29 11:15:43 +0200

    add homeController
    add / get
    add about page

--- exercises/routes.js
+++ exercises/routes.js
@@ -2,15 +2,13 @@ const router=require('express').Router();
 const genreController=require('./controllers/genreController');
 const authorController=require('./controllers/authorController');
 const bookController=require('./controllers/bookController');
-
+const homeController=require('./controllers/homeController');
 
 router.use('/genre',genreController);
 router.use('/author',authorController);
 router.use('/book',bookController);
+router.use(homeController)
 
-router.get('/',(req,res)=>{
-    res.render('home');
-});
 
 router.get('*', (req, res) => {
     res.render('404');
