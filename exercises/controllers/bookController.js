032826a07bbb9fb75fbfde2467025db4e797cab6
nikola.ninov <momzzze@abv.bg>
2024-01-29 13:41:39 +0200

    display all books

--- exercises/controllers/bookController.js
+++ exercises/controllers/bookController.js
@@ -3,6 +3,12 @@ const genreService = require('../service/genreService');
 const authorService = require('../service/authorService');
 const bookService = require('../service/bookService');
 
+
+router.get('/', async (req, res) => {
+    const books = await bookService.getAllBooks();
+    res.render('book/all', { books });
+})
+
 router.get('/create', async (req, res) => {
     const genres = await genreService.getAllGenres();
     const authors = await authorService.getAllAuthors();
