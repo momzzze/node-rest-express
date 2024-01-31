c338aa606c45d9384ea11b2c99a8bcacae2bc1a0
nikola.ninov <momzzze@abv.bg>
2024-01-31 18:40:46 +0200

    add register user to db

--- movie-magic-january-2024-main/movie-magic-january-2024-main/src/controllers/authController.js
+++ movie-magic-january-2024-main/movie-magic-january-2024-main/src/controllers/authController.js
@@ -1,9 +1,15 @@
-const router= require('express').Router();
+const router = require('express').Router();
+const authService = require('../services/authService');
+
 
 router.get('/register', (req, res) => {
     res.render('auth/register');
 });
-
+router.post('/register', async (req, res) => {
+    const body = req.body;
+    await authService.register(body)
+    res.redirect('/auth/login');
+});
 
 
 module.exports = router;
--- movie-magic-january-2024-main/movie-magic-january-2024-main/src/models/User.js
+++ movie-magic-january-2024-main/movie-magic-january-2024-main/src/models/User.js
@@ -1,31 +1,23 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: {
+    email: {
         type: String,
         required: true,
-        minlength: [5, 'Username should be at least 5 characters long'],
-        validate: {
-            validator: (value) => /^[A-Za-z0-9]+$/.test(value),
-            message: 'Username should consist only english letters and digits'
-        }
-    },
-    email:{
-        type: String,
-        required: true,
-        validate: {
-            validator: (value) => /^[A-Za-z0-9]+@[a-z]+\.[a-z]+$/.test(value),
-            message: 'Email should be valid'
-        }
+        lowercase: true,        
+        // validate: {
+        //     validator: (value) => /^[A-Za-z0-9]+@[a-z]+\.[a-z]+$/.test(value),
+        //     message: 'Email should be valid'
+        // }
     },
     password: {
         type: String,
         required: true,
-        minlength: [8, 'Password should be at least 8 characters long'],
-        validate: {
-            validator: (value) => /^[A-Za-z0-9]+$/.test(value),
-            message: 'Password should consist only english letters and digits'
-        }
+        // minlength: [8, 'Password should be at least 8 characters long'],
+        // validate: {
+        //     validator: (value) => /^[A-Za-z0-9]+$/.test(value),
+        //     message: 'Password should consist only english letters and digits'
+        // }
     }
 });
 
