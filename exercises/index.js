c7c71c5901ebe0bd3aa23b16b5808a49568b2402
nikola.ninov <momzzze@abv.bg>
2024-02-04 17:58:02 +0200

    add cookie parser

--- exercises/index.js
+++ exercises/index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = require('./routes');
 const handlebarsConfig = require('./config/handlebarsConfig');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 const app = express();
 require('dotenv').config();
 
 handlebarsConfig(app);
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cookieParser())
 app.use(router);
 
 mongoose.connect('mongodb://localhost:27017/bookList').then(() => {
